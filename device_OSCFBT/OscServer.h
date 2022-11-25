b6b6c8447bfd0d41492349bf609938e424514b35
公彦赤屋先 <kimihikoakaya@gmail.com>
2022-11-26 06:49:51 +0900

    Threads magic

--- device_OSCFBT/OscServer.h
+++ device_OSCFBT/OscServer.h
@@ -1,17 +1,18 @@
 #pragma once
 
+#include <mutex>
 #include <string>
 #include <Eigen/Dense>
 
 #include "miniosc.h"
 
+inline std::unique_lock<std::mutex> server_alive_mutex;
+
 class OscServer
 {
 public:
-    OscServer(const std::wstring& ipAddress, uint32_t port);
-
-    OscServer() = default;
-    ~OscServer();
+    explicit OscServer(const std::wstring& ipAddress, uint32_t port);
+    ~OscServer() = default;
 
 public:
     int Tick();
@@ -22,6 +23,8 @@ public:
     int SendPacket_Vector3(const std::string& address, float elem1, float elem2, float elem3);
     int SendPacket_Vector3(const std::string& address, Eigen::Vector3d vector);
     int SendPacket_Quat(const std::string& address, const Eigen::Quaterniond& orientation);
+
+    void Cleanup();
 
 private:
     static void Server_Callback(const char* address, const char* type, void** parameters);

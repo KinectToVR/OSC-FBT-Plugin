44e4227cdde40186ce04ef17cfc4cc8c966adb4e
公彦赤屋先 <kimihikoakaya@gmail.com>
2022-11-26 01:10:41 +0900

    C++ is a wonderful language

--- device_OSCFBT/OscHandler.h
+++ device_OSCFBT/OscHandler.h
@@ -9,69 +9,68 @@
 class OscHandler : public ktvr::K2TrackingDeviceBase_JointsBasis
 {
 public:
+    OscHandler()
+    {
+        deviceName = L"Amethyst OSC";
+        Flags_SettingsSupported = true;
+        trackedJoints.clear();
+    }
 
-	OscHandler() {
-		deviceName = L"Amethyst OSC";
-		Flags_SettingsSupported = true;
-		trackedJoints.clear();
-	}
+    ~OscHandler() override
+    {
+    }
 
-	virtual ~OscHandler()
-	{
-	}
-
-	std::wstring getDeviceGUID() override
-	{
-		// This ID is unique to this plugin!
-		return L"K2VRTEAM-VEND-API1-DVCE-OSCSPCTR";
-	}
+    std::wstring getDeviceGUID() override
+    {
+        // This ID is unique to this plugin!
+        return L"K2VRTEAM-VEND-API1-DVCE-OSCSPCTR";
+    }
 
 
-	HRESULT getStatusResult() override;
-	std::wstring statusResultWString(HRESULT stat) override;
+    HRESULT getStatusResult() override;
+    std::wstring statusResultWString(HRESULT stat) override;
 
-	void initialize() override;
-	void update() override;
-	void onLoad() override;
-	void shutdown() override;
-
-private:
-	void killServer();
+    void initialize() override;
+    void update() override;
+    void onLoad() override;
+    void shutdown() override;
 
 private:
+    void killServer();
 
-	OscServer m_server;
+private:
+    std::shared_ptr<OscServer> m_server;
 
-	bool m_hasBeenLoaded = false;
-	// bool m_serverIsRunning = false;
+    bool m_hasBeenLoaded = false;
+    // bool m_serverIsRunning = false;
 
-	std::vector<ktvr::ITrackedJointType> m_jointMapping;
+    std::vector<ktvr::ITrackedJointType> m_jointMapping;
 
-	// Default IP Address to stream OSC packets to. Currently defaults to localhost.
-	std::wstring m_net_target_ip_address = L"127.0.0.1";
-	// Default port to stream OSC packets on. Currently defaults to 9000.
-	uint32_t m_net_port = 9000;
+    // Default IP Address to stream OSC packets to. Currently defaults to localhost.
+    std::wstring m_net_target_ip_address = L"127.0.0.1";
+    // Default port to stream OSC packets on. Currently defaults to 9000.
+    uint32_t m_net_port = 9000;
 
-	// UI Elements
-	ktvr::Interface::TextBlock *m_ip_label_text_block, *m_port_label_text_block;
-	ktvr::Interface::TextBox *m_ip_text_box, * m_port_text_box;
-	ktvr::Interface::Button *m_connect_button;
+    // UI Elements
+    ktvr::Interface::TextBlock *m_ip_label_text_block, *m_port_label_text_block;
+    ktvr::Interface::TextBox *m_ip_text_box, *m_port_text_box;
+    ktvr::Interface::Button* m_connect_button;
 };
 
 /* Exported for dynamic linking */
 extern "C" __declspec(dllexport) void* TrackingDeviceBaseFactory(
-	const char* pVersionName, int* pReturnCode)
+    const char* pVersionName, int* pReturnCode)
 {
-	// Return the device handler for tracking
-	// but only if interfaces are the same / up-to-date
-	if (0 == strcmp(ktvr::IAME_API_Devices_Version, pVersionName))
-	{
-		static OscHandler TrackingHandler; // Create a new device handler -> KinectV2
+    // Return the device handler for tracking
+    // but only if interfaces are the same / up-to-date
+    if (0 == strcmp(ktvr::IAME_API_Devices_Version, pVersionName))
+    {
+        static OscHandler TrackingHandler; // Create a new device handler -> KinectV2
 
-		*pReturnCode = ktvr::K2InitError_None;
-		return &TrackingHandler;
-	}
+        *pReturnCode = ktvr::K2InitError_None;
+        return &TrackingHandler;
+    }
 
-	// Return code for initialization
-	*pReturnCode = ktvr::K2InitError_BadInterface;
+    // Return code for initialization
+    *pReturnCode = ktvr::K2InitError_BadInterface;
 }
--- device_OSCFBT/OscServer.h
+++ device_OSCFBT/OscServer.h
@@ -8,25 +8,25 @@
 class OscServer
 {
 public:
-	OscServer() = default;
-	OscServer(const std::wstring& ipAddress, uint32_t port);
-	~OscServer();
+    OscServer(const std::wstring& ipAddress, uint32_t port);
+
+    OscServer() = default;
+    ~OscServer();
 
 public:
-	int Tick();
-	void BeginPacket();
-	int FlushData();
-	bool IsAlive();
+    int Tick();
+    void BeginPacket();
+    int FlushData();
+    bool IsAlive();
 
-	int SendPacket_Vector3(const std::string& address, float elem1, float elem2, float elem3);
-	int SendPacket_Vector3(const std::string& address, Eigen::Vector3d vector);
-	int SendPacket_Quat(const std::string& address, Eigen::Quaterniond orientation);
-
-private:
-	static void Server_Callback(const char* address, const char* type, void** parameters);
+    int SendPacket_Vector3(const std::string& address, float elem1, float elem2, float elem3);
+    int SendPacket_Vector3(const std::string& address, Eigen::Vector3d vector);
+    int SendPacket_Quat(const std::string& address, const Eigen::Quaterniond& orientation);
 
 private:
-	mobundle m_packetBundle;
-	miniosc* m_oscServer;
+    static void Server_Callback(const char* address, const char* type, void** parameters);
+
+private:
+    mobundle m_packetBundle;
+    miniosc* m_oscServer;
 };
-
